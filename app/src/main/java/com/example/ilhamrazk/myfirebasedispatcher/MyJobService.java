462c8955ee94a262da9b678f195c7006d9d8cde2
Ilham Harazki <ilhamharazky@rocketmail.com>
2019-01-24 11:28:25 +0700

    First commit
    untuk pembelajaran

---
+++ app/src/main/java/com/example/ilhamrazk/myfirebasedispatcher/MyJobService.java
@@ -0,0 +1,160 @@
+package com.example.ilhamrazk.myfirebasedispatcher;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+
+import com.firebase.jobdispatcher.JobParameters;
+import com.firebase.jobdispatcher.JobService;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import org.json.JSONObject;
+
+import java.text.DecimalFormat;
+
+import cz.msebera.android.httpclient.Header;
+
+public class MyJobService extends JobService {
+    public static final String TAG = MyJobService.class.getSimpleName();
+
+    final String APP_ID = "4bc47c5e4d33cd17b14715249ec31aaa";
+
+    public static String EXTRAS_CITY = "extras_city";
+
+    /**
+     * onStartJob berjalan di dalam mainthread, return true jika ada proses yang membuat thread baru
+     */
+
+    @Override
+    public boolean onStartJob(JobParameters job) {
+        getCurrentWeather(job);
+
+        //return true ketika ingin dijalankan proses dengan thread yang berbeda, msial asynctask
+        return true;
+    }
+
+    /**
+     * onStopJob akan dipanggil ketika proses belum selesai dikarenakan contraint requirements tidak terpenuhi
+     * return true untuk re-scheduler
+     */
+
+    @Override
+    public boolean onStopJob(JobParameters job) {
+
+
+        //return true ketika ingin dijalankan proses dengan thread yang berbeda, msial asynctask
+        return true;
+    }
+
+    private void getCurrentWeather(final JobParameters job){
+
+        Bundle extras = job.getExtras();
+
+        //Lakukan pengecekan terlebih dahulu terhadap parameter job
+        if (extras == null){
+            jobFinished(job, false);
+            return;
+        } else if (extras.isEmpty()){
+            jobFinished(job, false);
+            return;
+        }
+
+        String city = extras.getString(EXTRAS_CITY);
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        String url = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+APP_ID;
+        client.get(url, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                String result = new String(responseBody);
+                Log.d(TAG, result);
+                try {
+                    JSONObject responseObject = new JSONObject(result);
+                    String currentWeather = responseObject.getJSONArray("weather").getJSONObject(0).getString("main");
+                    String description = responseObject.getJSONArray("weather").getJSONObject(0).getString("description");
+                    double tempInKelvin = responseObject.getJSONObject("main").getDouble("temp");
+
+                    double tempInCelcius = tempInKelvin - 273;
+                    String temprature = new DecimalFormat("##.##").format(tempInCelcius);
+
+                    String title = "Current Weather";
+                    String message = currentWeather + ", "+description+"with "+temprature+" celcius";
+                    int notifId = 100;
+
+                    showNotification(getApplicationContext(),title, message, notifId);
+
+                    // ketika proses selesai, maka perlu dipanggil jobFinished dengan parameter false;
+                    jobFinished(job, false);
+                }catch (Exception e){
+                    // ketika terjadi error, maka jobFinisihed diset denganparameter true. Yang artinya job perlu di reschedule
+                    jobFinished(job, true);
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                //ketika proses gagal, maka jobFinisher diset dengan parameter true. Yang artinya job perlu di reschedule
+                jobFinished(job,true);
+            }
+        });
+    }
+
+    /**
+     * Menampilkan datanya ke dalam notification
+     *
+     * @param context context dari notfication
+     * @param title judul notifikasi
+     * @param message isi dari notifikasi
+     * @param notifId id notifikasi
+     */
+    private void showNotification(Context context, String title, String message, int notifId){
+        String CHANNEL_ID = "Channel_1";
+        String CHANNEL_NAME = "Job service channel";
+
+        NotificationManager notificationManagerCompat = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setContentTitle(title)
+                .setSmallIcon(R.drawable.ic_replay_30_black_24dp)
+                .setContentText(message)
+                .setColor(ContextCompat.getColor(context, android.R.color.black))
+                .setVibrate(new long[]{1000, 1000, 1000, 1000, 1000})
+                .setSound(alarmSound);
+
+        /*
+        Untuk android Oreo ke atas perlu menambahkan notification channel
+         */
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+
+            /* Create or Update. */
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
+
+            channel.enableVibration(true);
+            channel.setVibrationPattern(new long[]{1000, 1000, 1000, 1000, 1000});
+
+            builder.setChannelId(CHANNEL_ID);
+
+            if (notificationManagerCompat != null){
+                notificationManagerCompat.createNotificationChannel(channel);
+            }
+        }
+
+        Notification notification = builder.build();
+
+        if (notificationManagerCompat != null){
+            notificationManagerCompat.notify(notifId, notification);
+        }
+    }
+
+
+}
